58d888a2c122f2783d4f3adc7a4b5a408d5496d1
“Richard <rich@Richards-Air.home>
2015-12-10 20:59:03 +0000

    Added test example

---
+++ test/example.cs
@@ -0,0 +1,24 @@
+
+
+using Xunit;
+using EmployeeApp.Controllers;
+using EmployeeApp.Models;
+using Moq;
+using Microsoft.AspNet.Mvc.ModelBinding;
+using Microsoft.AspNet.Mvc;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+	public class SomeExampleTests
+	{
+
+		[Fact]
+		public async Task ExampleTest()
+		{
+			Assert.Equal("Delete", "Delete");
+		}
+	}
+}
